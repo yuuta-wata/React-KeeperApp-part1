1861f17fbf7af53813c6934bdb0ca0f225a41d22
yuuta <yuuta_cookie@icloud.com>
2020-01-31 00:27:01 +0900

    add Note

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Note() {
+  return (
+    <div className="note">
+      <h1>This is the title</h1>
+      <p>This is the content</p>
+    </div>
+  );
+}
